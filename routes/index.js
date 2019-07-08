471a0089412c5698400bcf926ea1b4eccc86cefe
chris <crazychris134@gmail.com>
2019-07-08 10:45:43 +0530

    website commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome' });
+});
+router.get('/super',(req,res)=>{
+  res.render('super', { title: 'For'});
+});
+router.get('/app',(req,res)=>{
+  res.render('app', { title: 'About'});
+});
+router.get('/subs',(req,res)=>{
+  res.render('subs', { title: 'Hello'});
+});
+router.get('/grid',(req,res)=>{
+  res.render('grid', { title: 'Form'});
+});
+router.get('/expo',(req,res)=>{
+  res.render('expo', { title: 'Form'});
+});
+
+module.exports = router;
